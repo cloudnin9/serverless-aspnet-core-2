5496e96e30461e1c49e0ce09cc4bfb41c47c60fc
Naing Ye Lin Thein <nyl.thein@gmail.com>
2018-04-29 16:46:01 +1000

    updating the build.sh to include the sls commands to deploy app

--- src/cs-web-api/build.sh
+++ src/cs-web-api/build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
 #install zip
 apt-get -qq update
 apt-get -qq -y install zip
-
 dotnet restore
-
 #create deployment package
 dotnet lambda package --configuration release --framework netcoreapp2.0 --output-package bin/release/netcoreapp2.0/deploy-package.zip
+sls remove
+sls deploy --stage development
